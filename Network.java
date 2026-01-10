48838d5a5ed3b0a2521e29a6b9628659d53dbb06
Lear Eldar <lear.eldar@post.runi.ac.il>
2026-01-11 00:24:08 +0200

    Checking overall  - Network

--- Network.java
+++ Network.java
@@ -137,7 +137,7 @@ public class Network {
        //// Replace the following statement with your code
        String result = "Network:\n";
        
-       if (users == null) return "Network:";
+       if (userCount == 0) return "Network:";
 
        for (int i = 0; i < userCount; i++) {
            result += users[i];
