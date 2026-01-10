1e71b649006f5161e05b1cddc69a0ed9a1a8d95d
Lear Eldar <lear.eldar@post.runi.ac.il>
2026-01-11 00:12:47 +0200

    Checking overall  - Network

--- Network.java
+++ Network.java
@@ -137,7 +137,10 @@ public class Network {
        //// Replace the following statement with your code
        String result = "Network:\n";
        for (int i = 0; i < userCount; i++) {
-            result += users[i] + "\n";
+           result += users[i];
+                if (i < userCount - 1) {
+                    result += "\n";
+                }
        }
        return result;
     }
